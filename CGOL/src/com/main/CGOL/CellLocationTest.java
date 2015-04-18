1280d3b437eeaa7cc7d1db73e4b90abd8f06c8a8
Bill Winter <winter.william.m@gmail.com>
2015-04-18 13:44:21 -0400

    Testing Update

---
+++ CGOL/src/com/main/CGOL/CellLocationTest.java
@@ -0,0 +1,20 @@
+package com.main.CGOL;
+/**
+ * // -------------------------------------------------------------------------
+/**
+ * Tests the Cell location
+ *
+ * @author Parisa Samareh
+ * @author Bryan Kaperick
+ * @author William Winter
+ *  @version April 10, 2015
+ */
+public class CellLocationTest extends student.TestCase
+{
+
+
+    public void testLiveCell()
+    {
+        assertEquals(1, 1);
+    }
+}
---
+++ CGOL/src/com/main/CGOL/PlayScreenTest.java
@@ -0,0 +1,6 @@
+package com.main.CGOL;
+
+public class PlayScreenTest
+{
+
+}
